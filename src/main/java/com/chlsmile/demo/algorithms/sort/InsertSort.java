567498a62b36c20d3aa2e48c6de197d20dda5399
chlsmile <chl_smile@163.com>
2016-09-02 17:29:48 +0800

    add

--- src/main/java/com/chlsmile/demo/algorithms/sort/InsertSort.java
+++ src/main/java/com/chlsmile/demo/algorithms/sort/InsertSort.java
@@ -3,6 +3,17 @@ package com.chlsmile.demo.algorithms.sort;
 /**
  * Created with: IntelliJ IDEA.
  * Function:插入排序
+ *
+ * 1.算法介绍
+ *
+ * 2.适用场景
+ *
+ * 3.时间复杂度
+ *
+ * 4.优点
+ *
+ * 5.缺点
+ *
  * User: chl_smile
  * Date: 2016-09-02 17:18:00
  */
